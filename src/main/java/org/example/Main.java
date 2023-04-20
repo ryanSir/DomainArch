b47ecbc803749addfe2e0dd25dbbe9980362b072
ryanSir <1246120217@qq.com>
2023-04-20 11:08:36 +0800

    init

---
+++ src/main/java/org/example/Main.java
@@ -0,0 +1,13 @@
+package org.example;
+
+/**
+*
+*
+* @author ${USER}
+* @version Id: ${NAME}, v 0.1 ${DATE} ${TIME} ${USER} Exp $
+*/
+public class Main {
+    public static void main(String[] args) {
+        System.out.println("Hello world!");
+    }
+}
